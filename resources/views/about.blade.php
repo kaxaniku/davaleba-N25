e4e8d823075a804d818696f275f72167bd1c6dc6
kaxaniku <74068854+kaxaniku@users.noreply.github.com>
2022-04-08 17:11:13 +0400

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends ('layout')
+
+@section ('content')
+
+<main>about</main>
+
+@endsection
+
+@section('footer')
+    <p>about page footer</p>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends ('layout')
+
+@section ('content')
+
+<main>welcome</main>
+
+@endsection
+
+@section('footer')
+    <p>welcome page footer</p>
+@endsection
